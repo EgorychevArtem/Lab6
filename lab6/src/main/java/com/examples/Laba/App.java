8e1a26eb0d8e8afb794f843d5840b1a4ffcaec0b
Egorychev Artem <temych_2000@mail.ru>
2019-11-29 23:49:42 +0300

    Scripted auto-commit on change (2019-11-29 23:49:42) by gitwatch.sh

--- lab6/src/main/java/com/examples/Laba/App.java
+++ lab6/src/main/java/com/examples/Laba/App.java
@@ -1,5 +1,12 @@
 package com.examples.Laba;
 
-public class App {
+import java.io.IOException;
 
+public class App {
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        AsyncHttpClient httpClient = asyncHttpClient();
 }
