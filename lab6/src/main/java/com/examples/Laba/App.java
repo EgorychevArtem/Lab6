216eed70ad3b8fa8ad8f858ef1bee5c604618857
Egorychev Artem <temych_2000@mail.ru>
2019-11-29 23:54:09 +0300

    Scripted auto-commit on change (2019-11-29 23:54:09) by gitwatch.sh

--- lab6/src/main/java/com/examples/Laba/App.java
+++ lab6/src/main/java/com/examples/Laba/App.java
@@ -3,15 +3,17 @@ package com.examples.Laba;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
-import java.io.IOException;
+import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
+
+import java.io.IOException;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 public class App {
     public static void main(String[] args) throws IOException {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
-        final ZooKeeper zoo = new ZooKeeper(zookeeperConnectString, 3000, e -> log.info(e.toString()));
+        final ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, e -> log.info(e.toString()));
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
